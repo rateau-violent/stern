189edbf4d6e697e2c6e8e891adcff9525fc74936
Victoire Rabeau <victoire.rabeau@epitech.eu>
2025-10-04 15:31:47 +0200

    doc(stern/framework): Use doxygen-like inline documentation

--- lib/stern/include/stern/framework.h
+++ lib/stern/include/stern/framework.h
@@ -9,29 +9,29 @@ namespace stern {
      */
     template<typename T>
     concept Framework = requires(T t) {
-		// defines the framework's request type
+		/// defines the framework's request type
         typename T::request_type;
-		// defines the framework's response type
+		/// defines the framework's response type
         typename T::response_type;
-		// defines the framework's HTTP method type
+		/// defines the framework's HTTP method type
         typename T::method_type;
 
-		// the framework request_type must have a member variable named path, that must be a string
+		/// the framework request_type must have a member variable named path, that must be a string
         T::request_type::path;
         std::same_as<decltype(std::declval<typename T::request_type>().path), std::string>;
-		// the framework's request type must be constructible from a string
+		/// the framework's request type must be constructible from a string
         std::is_constructible_v<typename T::request_type, std::string>;
 
-		// the framework's request type must have a member named method, that must be the same type as the previously
-		// declared framework's method_type
+		/// the framework's request type must have a member named method, that must be the same type as the previously
+		/// declared framework's method_type
         T::request_type::method;
         std::same_as<decltype(std::declval<typename T::request_type>().method), typename T::method_type>;
 
-		// the framework must implement a function that returns the request's body as a json string
+		/// the framework must implement a function that returns the request's body as a json string
 		{ T::body_to_json(typename T::request_type("")) } -> std::same_as<std::string>;
-    	// the framework must implement a function that returns the request's query parameters as a json string
+    	/// the framework must implement a function that returns the request's query parameters as a json string
     	{ T::query_to_json(typename T::request_type("")) } -> std::same_as<std::string>;
-		// the framework must implement a function that returns the request's uri parameters as a json string
+		/// the framework must implement a function that returns the request's uri parameters as a json string
     	{ T::uri_to_json(typename T::request_type("")) } -> std::same_as<std::string>;
     };
 }
