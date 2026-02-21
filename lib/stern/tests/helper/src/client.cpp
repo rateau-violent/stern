30edd340383606bef92ebd11b58722f5bcb3acbd
Victoire Rabeau <victoire.rabeau@epitech.eu>
2026-02-21 18:06:50 +0100

    test(stern): Add POST method to curl client

--- lib/stern/tests/helper/src/client.cpp
+++ lib/stern/tests/helper/src/client.cpp
@@ -5,10 +5,17 @@
 
 #include "helper/client.h"
 
-namespace tests::helper {
+namespace {
+    size_t get_response_content(void *contents, std::size_t size, std::size_t nmemb, std::string *s) {
+        std::size_t newLength = size * nmemb;
 
-    client::client(const std::string& ip, std::size_t port, const std::string& logfile):
-                _ip{ip}, _port{port}, _logfile{logfile} {
+        s->append(static_cast<char*>(contents), newLength);
+        return newLength;
+    }
+}
+
+namespace tests::helper {
+    client::client(const std::string& ip, std::size_t port): _ip{ip}, _port{port} {
     }
 
     http::response client::get(const std::string& path) const {
@@ -25,7 +32,7 @@ namespace tests::helper {
 
         curl = curl_easy_init();
         if(curl) {
-            const std::string url = "http://" + _ip + ":" + std::to_string(_port) + "/" + path;
+            const std::string url = compute_url(path);
             curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 
             curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, get_response_content);
@@ -49,11 +56,65 @@ namespace tests::helper {
         return http::response(static_cast<http::codes>(response_code), response_content);
     }
 
-    size_t client::get_response_content(void *contents, std::size_t size, std::size_t nmemb, std::string *s) {
-        std::size_t newLength = size * nmemb;
+    http::response client::post(const std::string& path, const http::body_type& body) const {
+        CURL *curl;
+        long response_code;
+        std::string response_content;
 
-        s->append(static_cast<char*>(contents), newLength);
-        return newLength;
+        CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
+        if(result != CURLE_OK){
+            std::stringstream ss;
+            ss << "Curl error: " << "curl_global_init() failed:" << curl_easy_strerror(result);
+            throw std::runtime_error{ss.str()};
+        }
+
+        /* get a curl handle */
+        curl = curl_easy_init();
+        if(curl) {
+            std::string url = compute_url(path);
+
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+            auto body_len = body.size();
+            char* body_str = static_cast<char*>(malloc(body_len + 1));
+            std::memcpy(body_str, body.to_string().c_str(), body_len);
+            /* Now specify the POST data */
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, body_str);
+
+            /* Specify headers */
+            curl_slist *hs = nullptr;
+            hs = curl_slist_append(hs, std::string("Content-Length: " + std::to_string(body.size() + 2)).c_str());
+            if (body.is_json()) {
+                hs = curl_slist_append(hs, "Content-Type: application/json");
+            } else {
+                hs = curl_slist_append(hs, "Content-Type: text/html; charset=UTF-8");
+            }
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hs);
+
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, get_response_content);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_content);
+
+            /* Perform the request, result gets the return code */
+            result = curl_easy_perform(curl);
+            /* Check for errors */
+            if(result != CURLE_OK) {
+                std::stringstream ss;
+                ss << "Curl error: " << "curl_easy_perform() failed:" << curl_easy_strerror(result);
+                throw std::runtime_error{ss.str()};
+            }
+
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+
+            /* always cleanup */
+            curl_easy_cleanup(curl);
+            free(body_str);
+        }
+        curl_global_cleanup();
+        return http::response{static_cast<http::codes>(response_code), response_content};
     }
+
+    std::string client::compute_url(const std::string& path) const {
+        return "http://" + _ip + ":" + std::to_string(_port) + "/" + path;
+    }
+
 
 }
