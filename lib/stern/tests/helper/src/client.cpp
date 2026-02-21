bc89793fa285e6dec2ce19e18cc26c6a3c0b3d7b
Victoire Rabeau <victoire.rabeau@epitech.eu>
2026-02-21 22:24:55 +0100

    test(stern): Add DELETE method to curl client

--- lib/stern/tests/helper/src/client.cpp
+++ lib/stern/tests/helper/src/client.cpp
@@ -6,170 +6,189 @@
 #include "helper/client.h"
 
 namespace {
+class Curler {
+public:
+  explicit Curler() {
+    CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
+    if (result != CURLE_OK) {
+      throw std::runtime_error{make_error_message("curl_global_init", result)};
+    }
 
-    class Curler {
-        public:
-            explicit Curler() {
-                CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
-                if(result != CURLE_OK) {
-                    throw std::runtime_error{make_error_message("curl_global_init", result)};
-                }
+    _curl = curl_easy_init();
+  }
 
-                _curl = curl_easy_init();
-            }
+  ~Curler() {
+    if (_curl) {
+      curl_easy_cleanup(_curl);
+    }
+    curl_global_cleanup();
+  }
 
-            ~Curler() {
-                if (_curl) {
-                    curl_easy_cleanup(_curl);
-                }
-                curl_global_cleanup();
-            }
+  void get(const std::string& url, long& response_code, std::string& response_content) const {
+    curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
 
-            void get(const std::string& url, long& response_code, std::string& response_content) const {
-                curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
+    fill_response_content(response_content);
 
-                fill_response_content(response_content);
+    make_request();
 
-                make_request();
+    curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
+  }
 
-                curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
-            }
+  void post(const std::string& url, const http::body_type& body, long& response_code,
+            std::string& response_content) const {
+    curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
 
-            void post(const std::string& url, const http::body_type& body, long& response_code, std::string& response_content) const {
-                curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
+    std::size_t content_len;
+    char* raw_content = copy_request_body(body, content_len);
 
-                std::size_t content_len;
-                char* raw_content = copy_request_body(body, content_len);
+    /* Now specify the POST data */
+    curl_easy_setopt(_curl, CURLOPT_POSTFIELDS, raw_content);
 
-                /* Now specify the POST data */
-                curl_easy_setopt(_curl, CURLOPT_POSTFIELDS, raw_content);
+    /* Specify headers */
+    specify_headers({
+      {"Content-Length", std::to_string(body.size() + 2)},
+      {"Content-Type", body.is_json() ? "application/json" : "text/html; charset=UTF-8"}
+    });
 
-                /* Specify headers */
-                specify_headers({
-                    {"Content-Length", std::to_string(body.size() + 2)},
-                    {"Content-Type", body.is_json() ? "application/json" : "text/html; charset=UTF-8"}
-                });
+    fill_response_content(response_content);
 
-                fill_response_content(response_content);
+    make_request();
 
-                make_request();
+    curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
+    free(raw_content);
+  }
 
-                curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
-                free(raw_content);
-            }
-
-            void put(const std::string& url, const http::body_type& body, long& response_code, std::string& response_content) const {
-
-                curl_easy_setopt(_curl, CURLOPT_CUSTOMREQUEST, "PUT");
-                curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
+  void put(const std::string& url, const http::body_type& body, long& response_code,
+           std::string& response_content) const {
+    curl_easy_setopt(_curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
 
 
-                std::size_t content_len;
-                char* raw_content = copy_request_body(body, content_len);
+    std::size_t content_len;
+    char* raw_content = copy_request_body(body, content_len);
 
-                /* Now specify the PUT data */
-                curl_easy_setopt(_curl, CURLOPT_POSTFIELDS, raw_content);
+    /* Now specify the PUT data */
+    curl_easy_setopt(_curl, CURLOPT_POSTFIELDS, raw_content);
 
-                /* Specify headers */
-                specify_headers({
-                    {"Content-Length", std::to_string(body.size() + 2)},
-                    {"Content-Type", body.is_json() ? "application/json" : "text/html; charset=UTF-8"}
-                });
+    /* Specify headers */
+    specify_headers({
+      {"Content-Length", std::to_string(body.size() + 2)},
+      {"Content-Type", body.is_json() ? "application/json" : "text/html; charset=UTF-8"}
+    });
 
-                fill_response_content(response_content);
+    fill_response_content(response_content);
 
-                make_request();
-                curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
-                free(raw_content);
-            }
+    make_request();
+    curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
+    free(raw_content);
+  }
 
-        private:
-            CURL* _curl;
+  void del(const std::string& url, long& response_code, std::string& response_content) {
+    curl_easy_setopt(_curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
 
-            static std::string make_error_message(const std::string& fct_name, CURLcode code) {
-                std::stringstream ss;
+    fill_response_content(response_content);
 
-                ss << "Curl error: " << fct_name << "() failed:" << curl_easy_strerror(code) << ".";
-                return ss.str();
-            }
+    make_request();
+    curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &response_code);
+  }
 
-            static std::size_t get_response_content(void *contents, std::size_t size, std::size_t nmemb, std::string *s) {
-                std::size_t newLength = size * nmemb;
+private:
+  CURL* _curl;
 
-                s->append(static_cast<char*>(contents), newLength);
-                return newLength;
-            }
+  static std::string make_error_message(const std::string& fct_name, CURLcode code) {
+    std::stringstream ss;
 
-            static char* copy_request_body(const http::body_type& body, std::size_t& body_len) {
-                body_len = body.size();
-                char* body_str = static_cast<char*>(malloc(body_len + 1));
-                std::memcpy(body_str, body.to_string().c_str(), body_len);
-                return body_str;
-            }
+    ss << "Curl error: " << fct_name << "() failed:" << curl_easy_strerror(code) << ".";
+    return ss.str();
+  }
 
-            void specify_headers(const std::unordered_map<std::string, std::string>& headers) const {
-                curl_slist* hs = nullptr;
+  static std::size_t get_response_content(void* contents, std::size_t size, std::size_t nmemb, std::string* s) {
+    std::size_t newLength = size * nmemb;
 
-                for (const auto& [header, value]: headers) {
-                    hs = curl_slist_append(hs, std::string(header + ": " + value).c_str());
-                }
-                curl_easy_setopt(_curl, CURLOPT_HTTPHEADER, hs);
-            }
+    s->append(static_cast<char*>(contents), newLength);
+    return newLength;
+  }
 
-            void fill_response_content(std::string& response_content) const {
-                curl_easy_setopt(_curl, CURLOPT_WRITEFUNCTION, get_response_content);
-                curl_easy_setopt(_curl, CURLOPT_WRITEDATA, &response_content);
-            }
+  static char* copy_request_body(const http::body_type& body, std::size_t& body_len) {
+    body_len = body.size();
+    char* body_str = static_cast<char*>(malloc(body_len + 1));
+    std::memcpy(body_str, body.to_string().c_str(), body_len);
+    return body_str;
+  }
 
-            void make_request() const {
-                /* Perform the request, result gets the return code */
-                auto result = curl_easy_perform(_curl);
-                /* Check for errors */
-                if(result != CURLE_OK) {
-                    throw std::runtime_error{make_error_message("curl_easy_perform", result)};
-                }
-            }
-    };
+  void specify_headers(const std::unordered_map<std::string, std::string>& headers) const {
+    curl_slist* hs = nullptr;
 
+    for (const auto& [header, value] : headers) {
+      hs = curl_slist_append(hs, std::string(header + ": " + value).c_str());
+    }
+    curl_easy_setopt(_curl, CURLOPT_HTTPHEADER, hs);
+  }
+
+  void fill_response_content(std::string& response_content) const {
+    curl_easy_setopt(_curl, CURLOPT_WRITEFUNCTION, get_response_content);
+    curl_easy_setopt(_curl, CURLOPT_WRITEDATA, &response_content);
+  }
+
+  void make_request() const {
+    /* Perform the request, result gets the return code */
+    auto result = curl_easy_perform(_curl);
+    /* Check for errors */
+    if (result != CURLE_OK) {
+      throw std::runtime_error{make_error_message("curl_easy_perform", result)};
+    }
+  }
+};
 }
 
 namespace tests::helper {
-    client::client(const std::string& ip, std::size_t port): _ip{ip}, _port{port} {
-    }
+client::client(const std::string& ip, std::size_t port): _ip{ip}, _port{port} {
+}
 
-    http::response client::get(const std::string& path) const {
-        long response_code;
-        std::string response_content;
-        auto url = compute_url(path);
-        Curler curl;
+http::response client::get(const std::string& path) const {
+  long response_code;
+  std::string response_content;
+  auto url = compute_url(path);
+  Curler curl;
 
-        curl.get(url, response_code, response_content);
+  curl.get(url, response_code, response_content);
 
-        return http::response(static_cast<http::codes>(response_code), response_content);
-    }
+  return http::response(static_cast<http::codes>(response_code), response_content);
+}
 
-    http::response client::post(const std::string& path, const http::body_type& body) const {
-        long response_code;
-        std::string response_content;
-        Curler curl;
-        std::string url = compute_url(path);
+http::response client::post(const std::string& path, const http::body_type& body) const {
+  long response_code;
+  std::string response_content;
+  Curler curl;
+  std::string url = compute_url(path);
 
-        curl.post(url, body, response_code, response_content);
-        return http::response{static_cast<http::codes>(response_code), response_content};
-    }
+  curl.post(url, body, response_code, response_content);
+  return http::response{static_cast<http::codes>(response_code), response_content};
+}
 
-    http::response client::put(const std::string& path, const http::body_type& body) const {
-        long response_code;
-        std::string response_content;
-        Curler curl;
-        std::string url = compute_url(path);
+http::response client::put(const std::string& path, const http::body_type& body) const {
+  long response_code;
+  std::string response_content;
+  Curler curl;
+  std::string url = compute_url(path);
 
-        curl.put(url, body, response_code, response_content);
-        return http::response{static_cast<http::codes>(response_code), response_content};
-    }
+  curl.put(url, body, response_code, response_content);
+  return http::response{static_cast<http::codes>(response_code), response_content};
+}
 
-    std::string client::compute_url(const std::string& path) const {
-        return "http://" + _ip + ":" + std::to_string(_port) + "/" + path;
-    }
+http::response client::del(const std::string& path) const {
+  long response_code;
+  std::string response_content;
+  Curler curl;
+  std::string url = compute_url(path);
 
+  curl.del(url, response_code, response_content);
+  return http::response{static_cast<http::codes>(response_code), response_content};
+}
+
+
+std::string client::compute_url(const std::string& path) const {
+  return "http://" + _ip + ":" + std::to_string(_port) + "/" + path;
+}
 }
--- lib/stern/tests/test_integration.cpp
+++ lib/stern/tests/test_integration.cpp
@@ -6,64 +6,79 @@
 #include <criterion/criterion.h>
 
 void setup() {
-    test_fixture::get_instance().start_server();
+  test_fixture::get_instance().start_server();
 }
 
 void tear_down() {
-    test_fixture::get_instance().stop_server();
+  test_fixture::get_instance().stop_server();
 }
 
 Test(stern_integration_tests, base, .init = setup, .fini = tear_down) {
+  tests::helper::client client("127.0.0.1", 3000);
 
-    tests::helper::client client("127.0.0.1", 3000);
+  { // create a new user
+    auto res = client.post("users", http::body_type{
+                             nlohmann::json{
+                               {"first_name", "John"},
+                               {"last_name", "Doe"},
+                               {"age", 42}
+                             }
+                           });
 
-    { // post on /users route
-        auto res = client.post("users", http::body_type{nlohmann::json{
-            {"first_name", "John"},
-            {"last_name", "Doe"},
-            {"age", 42}
-            }});
+    cr_assert_eq(res.getCode(), http::codes::CREATED);
+  }
 
-        cr_assert_eq(res.getCode(), http::codes::CREATED);
-    }
+  { // get all users
+    auto res = client.get("users");
+    cr_assert_eq(res.getCode(), http::codes::OK);
+    cr_assert_eq(res.getBody().is_json(), true);
+    auto body = nlohmann::json::parse(res.getBody().to_string());
+    cr_assert_eq(body[0].at("first_name"), "John");
+    cr_assert_eq(body[0].at("last_name"), "Doe");
+    cr_assert_eq(body[0].at("age"), 42);
+  }
 
-    { // get all created users
-        auto res = client.get("users");
+  { // get one particular user
+    auto res = client.get("users?id=0");
+    cr_assert_eq(res.getCode(), http::codes::OK);
+    cr_assert_eq(res.getBody().is_json(), true);
+    auto body = nlohmann::json::parse(res.getBody().to_string());
+    cr_assert_eq(body.at("first_name"), "John");
+    cr_assert_eq(body.at("last_name"), "Doe");
+    cr_assert_eq(body.at("age"), 42);
+  }
 
-        cr_assert_eq(res.getCode(), http::codes::OK);
-        cr_assert_eq(res.getBody().is_json(), true);
-        auto body = nlohmann::json::parse(res.getBody().to_string());
-        cr_assert_eq(body[0].at("first_name"), "John");
-        cr_assert_eq(body[0].at("last_name"), "Doe");
-        cr_assert_eq(body[0].at("age"), 42);
-    }
+  { // Update user
+    auto res = client.put("users?id=0", http::body_type{
+                            nlohmann::json{
+                              {"first_name", "Isabelle"},
+                              {"last_name", "Durant"},
+                              {"age", 84}
+                            }
+                          });
+    cr_assert_eq(res.getCode(), http::codes::OK);
+  }
 
-    { // get one particular user
-        auto res = client.get("users?id=0");
-        cr_assert_eq(res.getCode(), http::codes::OK);
-        cr_assert_eq(res.getBody().is_json(), true);
-        auto body = nlohmann::json::parse(res.getBody().to_string());
-        cr_assert_eq(body.at("first_name"), "John");
-        cr_assert_eq(body.at("last_name"), "Doe");
-        cr_assert_eq(body.at("age"), 42);
-    }
+  { // get updated user
+    auto res = client.get("users?id=0");
+    cr_assert_eq(res.getCode(), http::codes::OK);
+    cr_assert_eq(res.getBody().is_json(), true);
+    auto body = nlohmann::json::parse(res.getBody().to_string());
+    cr_assert_eq(body.at("first_name"), "Isabelle");
+    cr_assert_eq(body.at("last_name"), "Durant");
+    cr_assert_eq(body.at("age"), 84);
+  }
 
-    { // Update user
-        auto res = client.put("users?id=0", http::body_type{nlohmann::json{
-            {"first_name", "Isabelle"},
-            {"last_name", "Durant"},
-            {"age", 84}
-        }});
-        cr_assert_eq(res.getCode(), http::codes::OK);
-    }
+  { // delete user
+    auto res = client.del("users?id=0");
+    cr_assert_eq(res.getCode(), http::codes::OK);
+  }
 
-    { // get updated user
-        auto res = client.get("users?id=0");
-        cr_assert_eq(res.getCode(), http::codes::OK);
-        cr_assert_eq(res.getBody().is_json(), true);
-        auto body = nlohmann::json::parse(res.getBody().to_string());
-        cr_assert_eq(body.at("first_name"), "Isabelle");
-        cr_assert_eq(body.at("last_name"), "Durant");
-        cr_assert_eq(body.at("age"), 84);
-    }
+  { // get all users
+    auto res = client.get("users");
+    cr_assert_eq(res.getCode(), http::codes::OK);
+    cr_assert_eq(res.getBody().is_json(), true);
+    auto body = nlohmann::json::parse(res.getBody().to_string());
+    cr_assert_eq(body.size(), 0);
+  }
 }
