cb7a495d6c10a6e7fa78b07fdb566e04b76dfa98
Victoire Rabeau <victoire.rabeau@epitech.eu>
2026-02-21 22:24:55 +0100

    test(stern): Add DELETE method to curl client

--- lib/stern/tests/helper/include/helper/client.h
+++ lib/stern/tests/helper/include/helper/client.h
@@ -4,28 +4,50 @@
 #include <http/response.h>
 
 namespace tests::helper {
-    class client {
-        public:
+/**
+ * Client that makes basic http requests
+ */
+class client {
+public:
+  /**
+   *  Creates a client object
+   *  @param [in] ip the server's ip
+   *  @param [in] port the server's port
+   */
+  explicit client(const std::string& ip, std::size_t port);
 
-            /**
-             *  Creates a client object
-             *  @param [in] ip the server's ip
-             *  @param [in] port the server's port
-             */
-            explicit client(const std::string& ip, std::size_t port);
+  /**
+   * Sends a http GET request to the server
+   * @param [in] path the path the request will be made on
+   */
+  [[nodiscard]] http::response get(const std::string& path) const;
 
-            [[nodiscard]] http::response get(const std::string& path) const;
-            [[nodiscard]] http::response post(const std::string& path, const http::body_type& body) const;
-            [[nodiscard]] http::response put(const std::string& path, const http::body_type& body) const;
+  /**
+   * Sends a http POST request to the server
+   * @param [in] path the path the request will be made on
+   * @param [in] body the request's body content
+   */
+  [[nodiscard]] http::response post(const std::string& path, const http::body_type& body) const;
 
-        private:
-            std::string _ip;
-            std::size_t _port;
+  /**
+   * Sends a http PUT request to the server
+   * @param [in] path the path the request will be made on
+   * @param [in] body the request's body content
+   */
+  [[nodiscard]] http::response put(const std::string& path, const http::body_type& body) const;
 
-            std::string compute_url(const std::string& path) const;
+  /**
+   * Sends a http DELETE request to the server
+   * @param [in] path the path the request will be made on
+   */
+  [[nodiscard]] http::response del(const std::string& path) const;
 
-    };
+private:
+  std::string _ip;
+  std::size_t _port;
+
+  std::string compute_url(const std::string& path) const;
+};
 }
 
 #endif /* STERN_TEST_CLIENT_H */
-
